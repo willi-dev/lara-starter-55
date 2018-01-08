dab6a23bd3e29a49ca4398cd3256d57e397da14b
willi-dev <willi.ilmukomputer@gmail.com>
2018-01-08 15:25:08 +0700

    update layour admin dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,65 @@ class HomeController extends Controller
     public function index( Request $request )
     {
         $request->user()->authorizeRoles(['admin', 'user']);
-        return view('home');
+        return view('admin.templates.home');
     }
+
+    public function templateEmail()
+    {
+        return view('admin.templates.email');
+    }
+
+    public function templateCompose()
+    {
+        return view('admin.templates.compose');
+    }
+
+    public function templateCalendar()
+    {
+        return view('admin.templates.calendar');
+    }
+
+    public function templateChat()
+    {
+        return view('admin.templates.chat');
+    }
+
+    public function templateChart()
+    {
+        return view('admin.templates.chart');
+    }
+
+    public function templateForm()
+    {
+        return view('admin.templates.form');
+    }
+
+    public function templateElement()
+    {
+        return view('admin.templates.element');
+    }
+
+    public function templateBasicTable()
+    {
+        return view('admin.templates.basictable');
+    }
+
+    public function templateDataTable()
+    {
+        return view('admin.templates.datatable');
+    }
+
+    public function templateGoogleMap()
+    {
+        return view('admin.templates.googlemaps');
+    }
+
+    public function templateVectorMap()
+    {
+        return view('admin.templates.vectormaps');
+    }
+
+
+
+
 }
---
+++ resources/views/admin/templates/vectormaps.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app-admin')
+
+@section('content-admin')
+<div>
+    @include('_partials.sidebar-left')
+    
+     <div class="page-container">
+        @include('_partials.header-nav')
+
+        <main class="main-content bgc-grey-100">
+            <div id="mainContent">
+                <div class="container-fluid">
+                    <h4 class="c-grey-900 mT-10 mB-30">Vector Maps</h4>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="bgc-white bd bdrs-3 p-20 mB-20">
+                                <h6 class="c-grey-900 mB-20">Jquery Vector Maps</h6>
+                                <div id="world-map-marker"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+        @include('_partials.footer-content')
+
+    </div>
+
+</div>
+
+@endsection
